04e402c27ca286e2247d6a382e99bb7ea07ed481
murilocarolino <123740523+murilocarolino@users.noreply.github.com>
2024-10-10 17:09:02 -0300

    implementação de carrossel

--- callMe/src/MenuScreen.jsx
+++ callMe/src/MenuScreen.jsx
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react"
 import Sidebar from './components/Sidebar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import 'bootstrap/dist/css/bootstrap.css'
+import Carousel from 'react-bootstrap/Carousel'
+
 
 const MenuScreen = () => {
+
   const [showPopSquare, setShowPopSquare] = useState(false)
   const [showInput, setShowInput] = useState(false);
 
@@ -27,9 +31,13 @@ const MenuScreen = () => {
     return () => clearTimeout(timer)
   }, [showPopSquare])
 
+
   return (
     <div className="menu-container">
+
       <Sidebar />
+
+      <img src="../img/voltar.png" alt="" className="voltar" />
 
       <div className="notas-container">
         <div className="nota3 nota"></div>
@@ -38,21 +46,21 @@ const MenuScreen = () => {
         <div className="pin"></div>
       </div>
 
+      <img src="../img/avancar.png" alt="" className="avancar" />
+
       <div className="botao-container">
         <button > <p>Postar Nota</p> </button>
         <button onClick={handleResponderClick}> <p>Responder Nota</p></button>
       </div>
 
-      <div className={`pop-square ${showPopSquare ? 'fade-in' : 'fade-out'}`}></div>
+      <div className={`pop-square ${showPopSquare ? 'fade-in' : 'fade-out'}`}>
+        <p className="popUp"> Ver sua resposta! </p>
+      </div>
 
       {showInput && (
         <div className="input-responder">
           <input className="responderInput" type="text" placeholder="Acrescente seu tópico..." />
-          <FontAwesomeIcon 
-            icon={faPaperPlane} 
-            className="icon-enviar" 
-            onClick={handleIconClick} 
-          />
+          <FontAwesomeIcon icon={faPaperPlane} className="icon-enviar" onClick={handleIconClick} />
         </div>
       )}
     </div>
