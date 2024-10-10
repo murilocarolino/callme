3fc3f53d8d88ecc39a23b9455252e8c0c5ed4365
MURILO CAROLINO DOS SANTOS <23111821@CFP127ALUNO.LOCAL>
2024-10-10 12:22:32 -0300

    página de notas adicionada

--- callMe/src/MenuScreen.jsx
+++ callMe/src/MenuScreen.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import Sidebar from './components/Sidebar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 const MenuScreen = () => {
+  const [showPopSquare, setShowPopSquare] = useState(false)
+  const [showInput, setShowInput] = useState(false);
+
+  const handleResponderClick = () => {
+    setShowInput(true)
+  }
+
+  const handleIconClick = () => {
+    setShowPopSquare(true)
+    setShowInput(false)
+  }
+
+  useEffect(() => {
+    let timer
+    if (showPopSquare) {
+      timer = setTimeout(() => {
+        setShowPopSquare(false)
+      }, 2000)
+    }
+
+    return () => clearTimeout(timer)
+  }, [showPopSquare])
+
   return (
-
     <div className="menu-container">
+      <Sidebar />
 
-      <div className="nota">
-        <div className="nota3"></div>
-        <div className="nota2"></div>
-        <div className="nota1"></div>
+      <div className="notas-container">
+        <div className="nota3 nota"></div>
+        <div className="nota2 nota"></div>
+        <div className="nota1 nota"></div>
         <div className="pin"></div>
       </div>
 
-      <div className="botao-container"> 
-        <button>Postar Nota</button>
-        <button>Responder Nota</button>
+      <div className="botao-container">
+        <button > <p>Postar Nota</p> </button>
+        <button onClick={handleResponderClick}> <p>Responder Nota</p></button>
       </div>
-      
+
+      <div className={`pop-square ${showPopSquare ? 'fade-in' : 'fade-out'}`}></div>
+
+      {showInput && (
+        <div className="input-responder">
+          <input className="responderInput" type="text" placeholder="Acrescente seu tópico..." />
+          <FontAwesomeIcon 
+            icon={faPaperPlane} 
+            className="icon-enviar" 
+            onClick={handleIconClick} 
+          />
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default MenuScreen;
--- callMe/src/components/Sidebar.jsx
+++ callMe/src/components/Sidebar.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import './Sidebar.css';
-import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
+
         <div className="menu">
 
-        <div className="notas">
-          <img src="./img/notas.png" alt="" className="icon" />
-        </div>
+          <div className="notas">
+            <img src="./img/notas.png" alt="" className="icon" />
+          </div>
 
-        <div className="chat">
-          <img src="./img/chat.png" alt="" className="icon" />
-        </div>
+          <div className="chat">
+            <img src="./img/chat.png" alt="" className="icon" />
+          </div>
 
-        <div className="autoajuda">
-          <img src="./img/autoajuda.png" alt="" className="icon" />
-        </div>
+          <div className="autoajuda">
+            <img src="./img/autoajuda.png" alt="" className="icon" />
+          </div>
 
-        <div className="estatistica">
-          <img src="./img/estatistica.png" alt="" className="icon"  />
-        </div>
+          <div className="estatistica">
+            <img src="./img/estatistica.png" alt="" className="icon"  />
+          </div>
 
-        <div className="doacao">
-          <img src="./img/doacao.png" alt="" className="icon"  />
-        </div>
+          <div className="doacao">
+            <img src="./img/doacao.png" alt="" className="icon"  />
+          </div>
 
       </div>
+      
     );
 };
 
