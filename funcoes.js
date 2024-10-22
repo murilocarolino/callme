42e0f6ad64f4fac698ce8947e06c25b36772e5b8
MURILO CAROLINO DOS SANTOS <23111821@CFP127ALUNO.LOCAL>
2024-10-22 17:28:12 -0300

    implementação com o back

---
+++ funcoes.js
@@ -0,0 +1,74 @@
+//const link = 'http://localhost:8080'
+// const link = 'https://acme-filmes-back.onrender.com'
+// const link = 'https://teste20241002-esh7gjgfc2hygtf0.brazilsouth-01.azurewebsites.net'
+const url = 'http://localhost:3000'
+const versao = 'v1'
+const link = `${url}/${versao}/callme`
+
+export async function getUsuarios(){
+    
+    const url = `${link}/usuarios`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.dados
+}
+
+export async function getNotas(params){
+    
+    let url = `${link}/notas?`
+    if(params.excluidas){
+        url += `&excluidas=${params.excluidas}`
+    }
+    if(params.quantity){
+        url += `&quantity=${params.quantity}`
+    }
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.dados
+}
+
+
+export async function postCliente(dados) {
+    const url = `${link}/cliente`;
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dados),
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json(); 
+      return { success: response.ok, data }; 
+    } catch (error) {
+      console.error('Erro ao enviar dados: ', error);
+      return { success: false, data: null }; 
+    }
+}
+
+export async function validateLogin(dados) {
+    const url = `${link}/login`;
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dados),
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json(); 
+      return { success: response.ok, data }; 
+    } catch (error) {
+      console.error('Erro ao enviar dados: ', error);
+      return { success: false, data: null }; 
+    }
+}
+
